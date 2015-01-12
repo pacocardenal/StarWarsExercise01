0df072ecfcf58c74aa249475f8dc853e4088ea5b
Paco Cardenal <pacocardenal@pacocardenal.com>
2015-01-12 10:20:09 +0100

    Initial Commit

---
+++ ResolucionEjercicio01/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ResolucionEjercicio01
+//
+//  Created by Paco on 12/1/15.
+//  Copyright (c) 2015 pacocardenal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
